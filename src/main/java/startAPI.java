e26d98ae889212fd318c91e3b257b9d346b4dd33
mohanad oweidat <mohanad.owidat@hotmail.com>
2021-12-07 13:39:07 +0100

    Added some dependencies to the project.

--- src/main/java/startAPI.java
+++ src/main/java/startAPI.java
@@ -1,7 +1,12 @@
+import org.apache.log4j.BasicConfigurator;
+
 public class startAPI
 {
 	public static void main(String[] args)
 	{
          //Vi startar API här.
+
+		//This is to enable logging.
+		BasicConfigurator.configure();
 	}
 }
